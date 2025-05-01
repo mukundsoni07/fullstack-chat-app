6762c2584d788b11b8bb4aeba2201254c4aca228
Mukund Soni <59203417+mukundsoni07@users.noreply.github.com>
2025-05-01 11:49:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
-const corsOption={
-    origin:'*'
-};
-app.use(cors(corsOption)); 
+app.use(cors({ origin: "*"}));
 
 //routes
 app.use("/api/v1/user",userRoute);
